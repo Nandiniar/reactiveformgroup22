9d1fd70e21dbbd249f49e824d1a3fcaf109cb26d
nandini <nandani@contaque.com>
2025-05-13 10:50:59 +0530

    Add this changes
    Signed-off-by: nandini <nandani@contaque.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup ,ReactiveFormsModule} from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'reactiveformgroup22';
+
+  profileForm =new  FormGroup({
+    name:new FormControl('Nandini'),
+    password:new FormControl('123'),
+    email:new FormControl('nandinitest.com')
+
+  })
+  onSubmit(){
+    console.log("on submit called")
+    console.log(this.profileForm.value)
+  }
+  setValue(){
+    this.profileForm.setValue(
+      {
+        name:'Nandi',
+password:"321",
+email:'nandi@test.com'
+      }
+    )
+  }
 }
